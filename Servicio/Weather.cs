4e7c73efd9c2a57b9369695702136cb0374d0409
jason1 <javendanob@intergrupo.com>
2020-07-16 15:37:06 -0500

    Add project files.

---
+++ Servicio/Weather.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BlazorAppDemo2.Servicio
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public int Grade { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
---
+++ Servicio/WeatherService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace BlazorAppDemo2.Servicio
+{
+    public interface IWeatherService
+    {
+        Task<Weather> GetProductAsync();
+    }
+
+    public class WeatherService : IWeatherService
+    {
+
+        public WeatherService()
+        {
+            this.client = httpClient;
+            client.BaseAddress = new Uri(@"http://localhost:44354/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        private HttpClient client;
+
+
+        public async Task<Weather> GetProductAsync()
+        {
+            Weather product = null;
+            HttpResponseMessage response = await client.GetAsync("WeatherForecast");
+            if (response.IsSuccessStatusCode)
+            {
+                var product1 = await response.Content.ReadAsStringAsync();
+                product = JsonConvert.DeserializeObject<Weather>(product1);
+            }
+            return product;
+        }
+    }
+}
